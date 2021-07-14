611225f9a034d1dfb1b8a5542d83d09f87d4472e
Md. Raihanul Karim <raihanulkarim5@gmail.com>
2021-07-14 23:12:56 +0600

    Bubble Sort has been Added

---
+++ BubbleSort/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfItem, temp, i;
+            Console.Write("Number of Items:");
+            numberOfItem = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = new int[numberOfItem];
+
+            Console.Write("Enter Values:");
+            for (i = 0; i < numberOfItem; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.Write("Unsorted Values:");
+            foreach (var item in arr)
+            {
+                Console.Write(item+" ");
+            }
+             //Bubble Sorting
+            for ( i = 0; i < numberOfItem-1; i++)
+            {
+                for (int j = 0; j < numberOfItem-i-1; j++)
+                {
+                    if (arr[j] > arr[j+1])
+                    {
+                        temp = arr[j];
+                        arr[j] = arr[j+1];
+                        arr[j+1] = temp;
+                    }
+                }
+            }
+            Console.Write("Sorted Values:");
+            foreach (var item in arr)
+            {
+                Console.Write(item+" ");
+            }
+           
+        }
+    }
+}
